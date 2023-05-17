6d3b99e9bc89d63c4010403630f6d07e3a240fa1
bmsacchi <bianca.sacchi@mail.utoronto.ca>
2023-05-17 09:17:04 -0400

    fixing old asereadcounter scripts

---
+++ asereadcounts.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+gatk ASEReadCounter \
+	-R NChap2_final.fa \
+	-I ../xyy_male/data/star_hap2/hyb/ScF2m12Aligned.sortedByCoord.out.bam \
+	-V ../xyy_male/data/variants_hap2/SCmale_f2_12_hap2_snps30.vcf \
+	-O SCF2m12rnacounts_qual30.table
+
+	 
---
+++ asereadcountsdna.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+gatk ASEReadCounter \
+	-R NChap2_final.fa \
+	-I ../xyy_male/data/bwa_hap2/SCmale_f2_12.sorted.bam \
+	-V ../xyy_male/data/variants_hap2/SCmale_f2_12_hap2_snps30.vcf \
+	-O SCF2m12dnacounts30.table
+
+	 
