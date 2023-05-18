2f4ff58c2706ae5435c1e6dfa22ae84a05b54914
bmsacchi <bianca.sacchi@mail.utoronto.ca>
2023-05-17 20:53:28 -0400

    readcounts

--- asereadcounts.sh
+++ asereadcounts.sh
@@ -2,8 +2,8 @@
 
 gatk ASEReadCounter \
 	-R NChap2_final.fa \
-	-I ../xyy_male/data/star_hap2/hyb/ScF2m12Aligned.sortedByCoord.out.bam \
-	-V ../xyy_male/data/variants_hap2/SCmale_f2_12_hap2_snps30.vcf \
+	-I ../xyy_male/data/star_hap2/hyb/ScF2m12Aligned.sortedByCoord.out.rg.bam \
+	-V ../xyy_male/data/variants_hap2/SCmale_f2_12_hap2_snps30.vcf.gz \
 	-O SCF2m12rnacounts_qual30.table
 
 	 
--- asereadcountsdna.sh
+++ asereadcountsdna.sh
@@ -2,8 +2,8 @@
 
 gatk ASEReadCounter \
 	-R NChap2_final.fa \
-	-I ../xyy_male/data/bwa_hap2/SCmale_f2_12.sorted.bam \
-	-V ../xyy_male/data/variants_hap2/SCmale_f2_12_hap2_snps30.vcf \
+	-I ../xyy_male/data/bwa_hap2/SCmale_f2_12.sorted.rg.bam \
+	-V ../xyy_male/data/variants_hap2/SCmale_f2_12_hap2_snps30.vcf.gz \
 	-O SCF2m12dnacounts30.table
 
 	 
